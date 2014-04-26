43396e4612bdf8338ad45bb15c675507dad2d9c1
Jon Peirce <jon.peirce@gmail.com>
2014-04-26 21:51:36 +0100

    ANALYSIS: Added a file to import and test Posner file

---
+++ analyse.py
@@ -0,0 +1,34 @@
+from os import path
+from psychopy import misc, gui
+import pandas as pd
+import scipy
+from scipy import stats
+
+#choose some data files to analyse
+filenames = gui.fileOpenDlg(allowed="*.csv")
+
+#loop through the files
+for thisFilename in filenames:
+    print thisFilename
+    thisDat = pd.read_csv(thisFilename)
+    
+    #filter out bad data
+    filtered = thisDat[ thisDat['rt']<=1.0 ]
+    filtered = filtered[ filtered['corr']==1 ]
+    
+    #separate conflicting from congruent reaction times
+    conflict = filtered[filtered.description == 'conflict']
+    congruent = filtered[filtered.description != 'conflict']
+    #get mean/std.dev
+    meanConfl = scipy.mean(conflict.rt)
+    semConfl = scipy.std(conflict.rt, ddof=1)
+    meanCongr = scipy.mean(congruent.rt)
+    semCongr = scipy.std(congruent.rt, ddof=1)
+    print "Conflict = %.3f (std=%.3f)" %(meanConfl, semConfl)
+    print "Congruent = %.3f (std=%.3f)" %(meanCongr, semCongr)
+    
+    #run a t-test
+    t, p = stats.ttest_ind(conflict.rt, congruent.rt)
+    print "Independent samples t-test: t=%.3f, p=%.4f" %(t, p)
+    
+    
