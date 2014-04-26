50f666edecc755e506dd3a2886eca775992d23e3
Jon Peirce <jon.peirce@gmail.com>
2014-04-26 16:47:33 +0100

    BASIC: Created fixation, cue and probe stimuli

--- posner.py
+++ posner.py
@@ -1,3 +1,22 @@
 from psychopy import visual, core, event, data
 
-win = visual.Window([1024,768], fullscr=False)
+win = visual.Window([1024,768], fullscr=False, units='pix')
+
+# initialise stimuli
+fixation = visual.Circle(win, size = 5,
+    lineColor = 'white', fillColor = 'lightGrey')
+probe = visual.ImageStim(win, size = 80, # 'size' is 3xSD for gauss,
+    pos = [300, 0], #we'll change this later
+    image = None, mask = 'gauss',
+    color = 'green')
+cue = visual.ShapeStim(win, 
+    vertices = [[-30,-20], [-30,20], [30,0]],
+    lineColor = 'red', fillColor = 'salmon')
+
+# run one trial
+fixation.draw()
+win.flip()
+cue.draw()
+win.flip()
+probe.draw()
+win.flip()
