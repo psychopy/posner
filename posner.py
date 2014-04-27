8222211bc03545e1a56754992e09c140497f8b04
Jonas Lindeløv <jonas@cnru.dk>
2014-04-27 16:54:25 +0200

    BETTER2: added runBlock function and practice trials

--- posner.py
+++ posner.py
@@ -2,7 +2,7 @@
 from psychopy import visual, core, event, data, gui, logging
 
 # Toggle debugging/production
-DEBUG = False
+DEBUG = True
 if DEBUG:
     fullscr = False
     logging.console.setLevel(logging.DEBUG)
@@ -42,75 +42,83 @@ cue = visual.ShapeStim(win,
     lineColor = 'red', fillColor = 'salmon')
 respClock = core.Clock()
 
-#set up the trials/experiment
-conditions = data.importConditions('conditions.csv') #import conditions from file
-trials = data.TrialHandler(trialList=conditions, nReps=5) #create trial handler (loop)
-#add trials to the experiment handler to store data
-thisExp = data.ExperimentHandler(
-        name='Posner', version='1.0', #not needed, just handy
-        extraInfo = info, #the info we created earlier
-        dataFileName = filename, # using our string with data/name_date
-        )
-thisExp.addLoop(trials) #there could be other loops (like practice loop)
-
-# Actually present stimuli: loop through trials
-for thisTrial in trials:
+# Run a block of trials. nReps is a positive integer and saveTrial is True/False.
+def runBlock(nReps, saveTrials=True):
+    #set up the trials/experiment
+    conditions = data.importConditions('conditions.csv') #import conditions from file
+    trials = data.TrialHandler(trialList=conditions, nReps=nReps) #create trial handler (loop)
+    #add trials to the experiment handler to store data
+    if saveTrials:
+        thisExp = data.ExperimentHandler(
+                name='Posner', version='1.0', #not needed, just handy
+                extraInfo = info, #the info we created earlier
+                dataFileName = filename, # using our string with data/name_date
+                )
+        thisExp.addLoop(trials) #there could be other loops (like practice loop)
     
-    # set up this trial
-    resp = None
-    rt = None
-    probe.setPos( [thisTrial['probeX'], 0] )
-    cue.setOri( thisTrial['cueOri'] )
+    # Actually present stimuli: loop through trials
+    for thisTrial in trials:
+        
+        # set up this trial
+        resp = None
+        rt = None
+        probe.setPos( [thisTrial['probeX'], 0] )
+        cue.setOri( thisTrial['cueOri'] )
+        
+        #fixation period
+        fixation.setAutoDraw(True)
+        for frameN in range(info['fixFrames']):
+            win.flip()
+            
+        #present cue
+        cue.setAutoDraw(True)
+        for frameN in range(info['cueFrames']):
+            win.flip()
+        cue.setAutoDraw(False)
     
-    #fixation period
-    fixation.setAutoDraw(True)
-    for frameN in range(info['fixFrames']):
+        #present probe and collect responses during presentation
+        probe.setAutoDraw(True)
+        win.callOnFlip(respClock.reset) #NB: reset not reset()
+        event.clearEvents()
+        for frameN in range(info['probeFrames']):
+            keys = event.getKeys(keyList = ['left','right','escape'])
+            if len(keys)>0:
+                resp = keys[0] #take the first keypress as the response
+                rt = respClock.getTime()
+                break #out of the probe-drawing loop
+            win.flip()
+        probe.setAutoDraw(False)
+        fixation.setAutoDraw(False)
+        
+        #clear screen
         win.flip()
         
-    #present cue
-    cue.setAutoDraw(True)
-    for frameN in range(info['cueFrames']):
-        win.flip()
-    cue.setAutoDraw(False)
-
-    #present probe and collect responses during presentation
-    probe.setAutoDraw(True)
-    win.callOnFlip(respClock.reset) #NB: reset not reset()
-    event.clearEvents()
-    for frameN in range(info['probeFrames']):
-        keys = event.getKeys(keyList = ['left','right','escape'])
-        if len(keys)>0:
-            resp = keys[0] #take the first keypress as the response
+        #wait for response if we didn't already have one
+        if resp is None:
+            keys = event.waitKeys(keyList = ['left','right','escape'])
+            resp = keys[0] #take first response
             rt = respClock.getTime()
-            break #out of the probe-drawing loop
-        win.flip()
-    probe.setAutoDraw(False)
-    fixation.setAutoDraw(False)
-    
-    #clear screen
-    win.flip()
-    
-    #wait for response if we didn't already have one
-    if resp is None:
-        keys = event.waitKeys(keyList = ['left','right','escape'])
-        resp = keys[0] #take first response
-        rt = respClock.getTime()
-    
-    #check if the response was correct
-    if thisTrial['probeX']>0 and resp=='right':
-        corr = 1
-    elif thisTrial['probeX']<0 and resp=='left':
-        corr = 1
-    elif resp=='escape':
-        corr = None
-        trials.finished = True
-    else:
-        corr = 0
         
-    #store the response and RT
-    trials.addData('resp', resp)
-    trials.addData('rt', rt)
-    trials.addData('corr', corr)
+        #check if the response was correct
+        if thisTrial['probeX']>0 and resp=='right':
+            corr = 1
+        elif thisTrial['probeX']<0 and resp=='left':
+            corr = 1
+        elif resp=='escape':
+            corr = None
+            trials.finished = True
+        else:
+            corr = 0
+        
+        
+        #store the response and RT
+        trials.addData('resp', resp)
+        trials.addData('rt', rt)
+        trials.addData('corr', corr)
+    
+        # Register end of trial in the log file
+        if saveTrials:
+            thisExp.nextEntry()
 
-    # Register end of trial in the log file
-    thisExp.nextEntry()
+runBlock(1, saveTrials=False)
+runBlock(5)
